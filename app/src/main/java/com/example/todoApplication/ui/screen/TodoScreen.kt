82cb9575070748a1bb83e645d25394b6f33927da
Ahmed Ikram <ikramahmed398@gmail.com>
2026-03-01 17:32:58 +0500

    feat: switch top bar to center aligned top bar

--- app/src/main/java/com/example/todoApplication/ui/screen/TodoScreen.kt
+++ app/src/main/java/com/example/todoApplication/ui/screen/TodoScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
@@ -11,7 +12,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -38,7 +38,7 @@ fun TodoScreen(vm: TodoViewModel = viewModel()) {
 
     Scaffold(
         topBar = {
-            TopAppBar(
+            CenterAlignedTopAppBar(
                 title = {
                     Text(text = stringResource(id = R.string.app_name))
                 },
