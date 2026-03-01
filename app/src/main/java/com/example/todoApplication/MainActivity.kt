b42896acacf4a92424ff12c7110b9dfe34dce859
Ahmed Ikram <ikramahmed398@gmail.com>
2026-03-01 17:45:52 +0500

    feat: updated preview name to TodoPreview

--- app/src/main/java/com/example/todoApplication/MainActivity.kt
+++ app/src/main/java/com/example/todoApplication/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TodoPreview() {
     TodoApplicationTheme {
         TodoScreen()
     }
